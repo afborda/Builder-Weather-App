aacbb07facf283c4d000766626edf6b0e974372d
ABNER BORDA FONSECA <abner.fonseca@C02CR37QMD6P.local>
2022-06-10 19:09:05 -0300

    add jest

---
+++ src/__tests__/screens/Home.spec.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+import Home from "../../screen/Home";
+import Main from "../../components/Main";
+
+test("check if show correctly button placeholder", () => {});
